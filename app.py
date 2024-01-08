c921d07c45ea1c5200e5a2dcb5268fdb35ef7689
vsnain <47077796+vsnain@users.noreply.github.com>
2024-01-07 22:51:57 -0500

    Update app.py

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ s3 = boto3.client('s3')
 # Use a specific path for the S3 object key
 S3_OBJECT_KEY = "some_files/job_count_data.json"
 
+
 def scrape_indeed_job_count():
     url = 'https://www.indeed.com/jobs?q=software+engineer&sort=date&fromage=1'
 
@@ -21,16 +22,14 @@ def scrape_indeed_job_count():
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
 
-    # Use the CSS selector to find the span element containing the job count
-    job_count_element = soup.select('div.jobsearch-JobCountAndSortPane-jobCount').find_next('span').text
-    print("job_count_element")
-    print(job_count_element)
-    # Check if the element exists
-    if job_count_element:
-        job_count_text = job_count_element.text.strip()
+    # Look for span elements with a text that contains 'Jobs'
+    job_count_elements = soup.find_all('span', text=lambda text: text and 'Jobs' in text)
+    print(job_count_elements)
+    # Iterate through job count elements and try to find the pattern 'number in 100s Jobs'
+    for job_count_element in job_count_elements:
+        job_count_text = job_count_element.get_text(strip=True)
         
         # Extract the number of jobs using regular expressions
-        import re
         match = re.search(r'\d+', job_count_text)
         if match:
             job_count = int(match.group())
@@ -38,6 +37,7 @@ def scrape_indeed_job_count():
 
     # If the element is not found, return 0
     return 0
+
 
 def save_job_count_to_s3(job_count, timestamp):
     # Get existing data from S3
